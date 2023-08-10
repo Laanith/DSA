59687ae4ec5480fda3a486be4e0916e9647986b8
Laanith <lanithchouhan2004@gmail.com>
2023-08-10 23:26:18 +0530

    Latest Commit

--- HeapsandPriorityQueues/Easy/KLargestElements_IB.cpp
+++ HeapsandPriorityQueues/Easy/KLargestElements_IB.cpp
@@ -8,8 +8,15 @@
 #include<assert.h>
 #define PYES cout<<"YES"<<endl
 #define PNO cout<<"NO"<<endl
- 
- 
+
+/*!SECTION : Problem description
+Given an 1D integer array A of size N you have to find and return the B largest elements of the array A.
+
+NOTE:
+
+Return the largest B elements in any order you like.
+*/
+
 using namespace std;
 
 class Solution{
