90c91939ee9383675014191f0fddd515052a9551
Laanith <lanithchouhan2004@gmail.com>
2023-08-12 09:31:48 +0530

    Latest Commit

--- gitscript.bat
+++ gitscript.bat
@@ -1,3 +1,16 @@
+@echo off
+setlocal
+
+set "targetFolder=D:\Code\DSA"
+
+:DeleteFiles
+for /r "%targetFolder%" %%F in (*.exe) do (
+    echo Deleting "%%F"
+    del "%%F" /q
+)
+
+endlocal
+
 git add -A
 
 git commit -m "Latest Commit"
