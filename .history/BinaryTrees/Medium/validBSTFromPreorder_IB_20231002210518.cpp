3ed0665083608853d772a76885fc78a455eb12e3
Laanith <lanithchouhan2004@gmail.com>
2023-10-02 21:07:54 +0530

    Latest Commit

---
+++ .history/BinaryTrees/Medium/validBSTFromPreorder_IB_20231002210518.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define si stack<int>
+#define qi queue<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+#define umapii unordered_map<int,int>
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvs(vs A){cout<<endl;for(auto x : A) cout<<x<<" ";cout<<endl;}
+
+
+
+/*!SECTION : Problem Description ::
+You are given a preorder traversal A, of a Binary Search Tree.
+
+Find if it is a valid preorder traversal of a BST.
+
+Note: Binary Search Tree by definition has distinct keys and duplicates in binary search tree are not allowed.
+
+Input 1:
+
+A = [7, 7, 10, 10, 9, 5, 2, 8]
+
+Output 1:
+
+ 0
+
+*/
+
+
+
+class Solution{
+    public :
+        int solve(vi &A);
+};
+
+
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+
+
+return 0 ;
+}
