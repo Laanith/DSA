dc248154fd932e2cd1820e7205055398a08c0de7
Laanith <lanithchouhan2004@gmail.com>
2023-10-09 22:05:23 +0530

    Latest Commit

---
+++ .history/BinaryTrees/Medium/LeastCommonAncestor_IB_20231009215108.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define si stack<int>
+#define qi queue<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+#define umapii unordered_map<int,int>
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvs(vs A){cout<<endl;for(auto x : A) cout<<x<<" ";cout<<endl;}
+
+
+
+/*!SECTION : Problem Description ::
+Find the lowest common ancestor in an unordered binary tree given two values in the tree.
+
+Lowest common ancestor: the lowest common ancestor (LCA) of two nodes v and w in a tree or directed acyclic graph (DAG) is the lowest (i.e. deepest) node that has both v and w as descendants.
+
+Note:
+You are given 2 values. Find the lowest common ancestor of the two nodes represented by val1 and val2
+No guarantee that val1 and val2 exist in the tree. If one value doesn't exist in the tree then return -1.
+There are no duplicate values.
+You can use extra memory, and helper functions, and can modify the node struct but, you can't add a parent pointer.
+
+*/
+
+
+
+class Solution{
+    public :
+        
+};
+
+
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+
+
+return 0 ;
+}
