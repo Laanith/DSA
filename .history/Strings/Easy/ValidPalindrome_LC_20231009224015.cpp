976f06d038c0af5bb0f9024d10c9dae10a742e73
Laanith <lanithchouhan2004@gmail.com>
2023-10-09 22:45:13 +0530

    Latest Commit

---
+++ .history/Strings/Easy/ValidPalindrome_LC_20231009224015.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define si stack<int>
+#define qi queue<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+#define umapii unordered_map<int,int>
+#define vvi vector<vector<int>>
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvs(vs A){cout<<endl;for(auto x : A) cout<<x<<" ";cout<<endl;}
+void ps(string A){cout<<A<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}}
+
+
+
+/*!SECTION : Problem Description ::
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.    
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+*/
+
+
+
+class Solution{
+    public :
+        bool validPalindrome(string s);
+};
+
+
+bool Solution::validPalindrome(string s){
+    auto x = ""l
+}
+
+
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+
+
+return 0 ;
+}
