2b33a93b10ec32470b7617596bb910cec9fd2db1
Laanith <lanithchouhan2004@gmail.com>
2023-10-10 21:43:57 +0530

    Latest Commit

---
+++ .history/Arrays/Easy/IntersectionOfSortedArrays_IB_20231010213845.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define si stack<int>
+#define qi queue<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+#define umapii unordered_map<int,int>
+#define vvi vector<vector<int>>
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvs(vs A){cout<<endl;for(auto x : A) cout<<x<<" ";cout<<endl;}
+void ps(string A){cout<<A<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}}
+
+
+
+/*!SECTION : Problem Description ::
+Find the intersection of two sorted arrays OR in other words, given 2 sorted arrays, find all the elements which occur in both arrays.
+
+NOTE: For the purpose of this problem ( as also conveyed by the sample case ), assume that elements that appear more than once in both arrays should be included multiple times in the final output.
+
+*/
+
+
+
+class Solution{
+    public :
+         
+};
+
+
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+
+
+return 0 ;
+}
