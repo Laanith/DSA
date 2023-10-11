6e77af87a305c9c102137e3bf03ce578923e74ba
Laanith <lanithchouhan2004@gmail.com>
2023-10-11 23:10:19 +0530

    Latest Commit

---
+++ Arrays/Easy/PickFromBothSides_IB.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define si stack<int>
+#define qi queue<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+#define umapii unordered_map<int,int>
+#define vvi vector<vector<int>>
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvs(vs A){cout<<endl;for(auto x : A) cout<<x<<" ";cout<<endl;}
+void ps(string A){cout<<A<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}}
+
+
+
+/*!SECTION : Problem Description ::
+Given an integer array A of size N.
+
+You have to pick exactly B elements from either left or right end of the array A to get the maximum sum.
+
+Find and return this maximum possible sum.
+
+NOTE: Suppose B = 4 and array A contains 10 elements then
+
+You can pick the first four elements or can pick the last four elements or can pick 1 from the front and 3 from the back etc.
+ you need to return the maximum possible sum of elements you can pick.
+
+ Input 1:
+
+ A = [5, -2, 3 , 1, 2]
+ B = 3
+
+ Output 1:
+
+ 8
+
+*/
+
+
+
+class Solution{
+    public :
+
+    int pickFromBothSides(vi &A, int B);
+        
+};
+
+
+int Solution::pickFromBothSides(vi &A, int B){
+    int i = 0 , j = A.size()-1;
+    int n1 = A.size();
+    vi pre; 
+    int sum = 0 ;
+    for(auto x : A){
+        sum += x;
+        pre.push_back(sum);
+    }
+
+    int ans = -1e8;
+    sum = 0;
+    for(int i = 0 ; i < B; i++){
+        ans = max(ans, sum + pre[B-i-1]);
+        sum += A[n1-i-1];
+    }
+
+    return ans;
+}
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi A = {5,-2,3,1,2};
+pint(S->pickFromBothSides(A,3));
+
+return 0 ;
+}
