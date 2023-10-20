abf40406c15f79b3ceb61529d0f85a8f2e29651f
Laanith <lanithchouhan2004@gmail.com>
2023-10-20 21:31:31 +0530

    Latest Commit

---
+++ .history/NumberTheoryandMath/Easy/Oct202023_GFG_20231020212600.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+You will be given an array arr of integers of length N. You can construct an integer from two integers by treating the integers as strings, and then concatenating them. For example, 19 and 4 can be used to construct 194 and 419.
+
+The task is to find whether it's possible to construct an integer using all the digits of these numbers such that it would be divisible by 3.
+If it is possible then print 1 and if not print 0.
+
+
+Input: N = 3
+arr = {40, 50, 90}
+Output: 1
+Explanation: One such number is 405090
+*/
+
+class Solution{
+public :
+
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+ 
+ 
+return 0 ;
+}
