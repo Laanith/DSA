8848b495d2dbdc61136329d22678251613a74801
Laanith <lanithchouhan2004@gmail.com>
2023-10-20 21:17:39 +0530

    Latest Commit

---
+++ .history/Strings/Medium/Salutes_IB_20231020211445.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+ In a long hallway some soldiers are walking from left to right and some from right to left all at the same speed.
+
+Every time while walking they cross through another soldier they salute and move ahead.
+
+Given a string A of length N showing the soldiers' direction they are walking.
+'<' denotes a soldier is walking from right to left, and '>' denotes a soldier is walking from left to right.
+Return the number of Salutes done.
+*/
+
+class Solution{
+public :
+    long Salutes(string s);
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+ 
+ 
+return 0 ;
+}
