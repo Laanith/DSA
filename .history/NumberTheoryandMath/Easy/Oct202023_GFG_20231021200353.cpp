f113a06f82301d5435927336b20b4c09268207c5
Laanith <lanithchouhan2004@gmail.com>
2023-10-21 23:08:12 +0530

    Latest Commit

--- .history/NumberTheoryandMath/Easy/Oct202023_GFG_20231021200353.cpp
+++ .history/NumberTheoryandMath/Easy/Oct202023_GFG_20231021200353.cpp
@@ -1,29 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define ll long long
-#include <algorithm>
+#include<algorithm>
 #define vi vector<int>
-#define vpi vector<pair<int, int>>
-#define PYES cout << "YES" << endl
-#define PNO cout << "NO" << endl
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
 #define vs vector<string>
 #define qi queue<int>
 #define si stack<int>
-#define pi pair<int, int>
+#define pi pair<int,int>
 #define ima INT_MAX
 #define imi INT_MIN
 #define siz size()
 #define pb push_back()
 using namespace std;
-void pv(vi A)
-{
-    cout << endl;
-    for (auto x : A)
-        cout << x << ' ';
-    cout << endl;
-}
-void pint(int A) { cout << endl
-                        << A << endl; }
-void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
 
 /*!SECTION : Problem Description ::
 You will be given an array arr of integers of length N. You can construct an integer from two integers by treating the integers as strings, and then concatenating them. For example, 19 and 4 can be used to construct 194 and 419.
@@ -38,30 +31,27 @@ Output: 1
 Explanation: One such number is 405090
 */
 
-class Solution
-{
-public:
+class Solution{
+public :
     int isPossible(vi A);
 };
-
-int Solution::isPossible(vi A)
-{
+ 
+int Solution::isPossible(vi A){
     int rem = 0;
-    for (auto num : A)
-    {
+    for(auto num : A){
         rem = ((rem + num) % 3);
     }
 
     return rem == 0;
 }
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    auto S = new Solution();
-    vi A = {3, 6};
-    pint(S->isPossible(A));
 
-    return 0;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi A = {3, 6};
+pint(S->isPossible(A));
+
+return 0 ;
 }
