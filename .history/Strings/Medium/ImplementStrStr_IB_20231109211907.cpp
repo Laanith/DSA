bb33c0556a4b3495fbcdf2a70361b52ca9e4ff94
Laanith <lanithchouhan2004@gmail.com>
2023-11-09 21:23:23 +0530

    Latest Commit

---
+++ .history/Strings/Medium/ImplementStrStr_IB_20231109211907.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+
+ Problem Description
+
+
+
+Another question which belongs to the category of questions which are intentionally stated vaguely.
+
+Expectation is that you will ask for correct clarification or you will state your assumptions before you start coding.
+
+Implement strStr().
+
+strstr - locate a substring ( needle ) in a string ( haystack ).
+
+Try not to use standard library string functions for this question.
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+NOTE: String A is haystack, B is needle.
+
+Good clarification questions:
+
+What should be the return value if the needle is empty?
+What if both haystack and needle are empty?
+For the purpose of this problem, assume that the return value should be -1 in both cases.
+*/
+
+class Solution{
+public :
+    int solve(const string A, const string B);
+};
+ 
+ 
+int Solution::solve(const string A, const string B){
+    int n = A.size();
+    int m = B.size();
+}
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+ 
+ 
+return 0 ;
+}
