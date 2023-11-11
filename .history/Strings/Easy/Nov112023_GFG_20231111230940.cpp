255090a85dea321a4f464dcdd9777672996f494b
Laanith <lanithchouhan2004@gmail.com>
2023-11-11 23:10:54 +0530

    Latest Commit

---
+++ .history/Strings/Easy/Nov112023_GFG_20231111230940.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+Given two strings 'str1' and 'str2', check if these two strings are isomorphic to each other.
+
+If the characters in str1 can be changed to get str2, then two strings, str1 and str2, are isomorphic. A character must be completely swapped out for another character while maintaining the order of the characters. A character may map to itself, but no two characters may map to the same character.
+
+Example 1:
+
+Input:
+str1 = aab
+str2 = xxy
+Output:
+1
+Explanation:
+There are two different characters in aab and xxy, i.e a and b with frequency 2 and 1 respectively.
+Example 2:
+
+Input:
+str1 = aab
+str2 = xyz
+Output:
+0
+Explanation:
+There are two different characters in aab but there are three different charactersin xyz. So there won't be one to one mapping between str1 and str2.
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function areIsomorphic() which takes the string str1 and string str2 as input parameter and  check if two strings are isomorphic. The function returns true if strings are isomorphic else it returns false.
+
+Expected Time Complexity: O(|str1|+|str2|).
+Expected Auxiliary Space: O(Number of different characters).
+Note: |s| represents the length of string s.
+
+Constraints:
+1 <= |str1|, |str2| <= 105
+*/
+
+class Solution{
+public :
+    bool solve(string A, string B);
+};
+
+
+bool Solution::solve(string A, string B){
+    if(A.length() != B.length())
+        return false;
+
+    map<char, char> m;
+    set<char> setOfBCharacters;
+
+    for (int i = 0; i < A.size(); i++){
+        char a = A[i];
+        char b = B[i];
+
+        if(m.find(a) != m.end()){
+            if(m[a] !=b)
+                return false;
+        }
+
+        else {
+            if(setOfBCharacters.find(b) != setOfBCharacters.end()){
+                return false;
+            }
+        }
+
+        m[a] = b;
+        setOfBCharacters.insert(b);
+    }
+
+    return true;
+}
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+pint(S->solve("abcddd", "defxxx"));
+
+return 0 ;
+}
