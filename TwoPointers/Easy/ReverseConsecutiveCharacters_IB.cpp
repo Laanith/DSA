730d6fa7265600e90caab289eae54291e26ca7e1
Laanith <lanithchouhan2004@gmail.com>
2023-11-18 00:13:00 +0530

    Latest Commit

---
+++ TwoPointers/Easy/ReverseConsecutiveCharacters_IB.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+Problem Description
+
+
+
+Given a string A and integer B, remove all consecutive same characters that have length exactly B.
+
+
+NOTE : All the consecutive same characters that have length exactly B will be removed simultaneously.
+
+
+
+Problem Constraints
+1 <= |A| <= 100000
+
+1 <= B <= |A|
+
+
+
+Input Format
+First Argument is string A.
+
+Second argument is integer B.
+
+
+
+Output Format
+Return a string after doing the removals.
+
+
+
+Example Input
+Input 1:
+
+A = "aabcd"
+B = 2
+Input 2:
+
+A = "aabbccd"
+B = 2
+
+
+Example Output
+Output 1:
+
+ "bcd"
+Output 2:
+
+ "d"
+
+
+Example Explanation
+Explanation 1:
+
+ "aa" had length 2.
+Explanation 2:
+
+ "aa", "bb" and "cc" had length of 2.
+
+
+*/
+
+class Solution{
+public :
+    string solve(string A, int B);
+};
+ 
+string Solution::solve(string A, int B){
+    string ans = "";
+    int j = 0, n = A.size();
+
+    while (j < n)
+    {
+        int i = j;
+        int count = 0;
+        string temp = "";
+        while(i < n && A[i] == A[j]){
+            count++;
+            temp += A[i];
+            i++;
+        }
+        if(count!=B){
+            ans += temp;
+        }
+
+        j = i;
+    }
+
+    return ans;
+}
+
+ 
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout << S->solve("aabccaaddvvadd", 2);
+
+return 0 ;
+}
