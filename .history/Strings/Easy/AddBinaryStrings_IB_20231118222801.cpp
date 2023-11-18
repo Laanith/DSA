fa191a1cfbc39033557cddc6e2dd64001b5e9333
Laanith <lanithchouhan2004@gmail.com>
2023-11-18 22:32:03 +0530

    Latest Commit

---
+++ .history/Strings/Easy/AddBinaryStrings_IB_20231118222801.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+Problem Description
+
+
+
+Given two binary strings A and B. Return their sum (also a binary string).
+
+
+Problem Constraints
+1 <= length of A <= 105
+
+1 <= length of B <= 105
+
+A and B are binary strings
+
+
+
+Input Format
+The two argument A and B are binary strings.
+
+
+
+Output Format
+Return a binary string denoting the sum of A and B
+
+
+
+Example Input
+Input 1:
+A = "100"
+B = "11"
+Input 2:
+A = "110"
+B = "10"
+
+
+Example Output
+Output 1:
+"111"
+Output 2:
+"1000"
+
+
+Example Explanation
+For Input 1:
+The sum of 100 and 11 is 111.
+For Input 2:
+
+The sum of 110 and 10 is 1000.
+
+
+*/
+
+class Solution{
+public :
+
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+ 
+ 
+return 0 ;
+}
---
+++ .history/Strings/Easy/AddBinaryStrings_IB_20231118223135.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+Problem Description
+
+
+
+Given two binary strings A and B. Return their sum (also a binary string).
+
+
+Problem Constraints
+1 <= length of A <= 105
+
+1 <= length of B <= 105
+
+A and B are binary strings
+
+
+
+Input Format
+The two argument A and B are binary strings.
+
+
+
+Output Format
+Return a binary string denoting the sum of A and B
+
+
+
+Example Input
+Input 1:
+A = "100"
+B = "11"
+Input 2:
+A = "110"
+B = "10"
+
+
+Example Output
+Output 1:
+"111"
+Output 2:
+"1000"
+
+
+Example Explanation
+For Input 1:
+The sum of 100 and 11 is 111.
+For Input 2:
+
+The sum of 110 and 10 is 1000.
+
+
+*/
+
+class Solution{
+public :
+    string addBinaries(string A, string B);
+};
+ 
+string Solution::addBinaries(string A, string B){
+    int i = A.length() - 1;
+    int j = B.length() - 1;
+    int carry = 0;
+    string ans = "";
+
+    while(i >= 0 || j >= 0 || carry != 0){
+        int sum = 0;
+        if( i >= 0){
+            sum += (A[i] - '0');
+            i--;
+        }
+        if(j >= 0){
+            sum += (B[j] - '0');
+            j--;
+        }
+        sum += carry;
+        ans = to_string(sum % 2) + ans;
+        carry = sum / 2;
+    }
+    return ans;
+}
+
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout << S.solve("110", "10");
+
+return 0 ;
+}
