72c8f34b6c3f882ef2b1e111a21460e4cbeabddc
Laanith <lanithchouhan2004@gmail.com>
2024-02-20 20:02:36 +0530

    Latest Commit

---
+++ .history/Strings/Easy/FindTheIndexOfTheFirstOccurrenceInAString_LC_20240220200202.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+class Solution
+{
+public:
+int strStr(string haystack,string needle);
+};
+
+int Solution::strStr(string haystack,string needle){
+    int n = haystack.length();
+    int m = needle.length();
+    for(int r=0; r < n-m+1;r++){
+        if(needle == haystack.substr(r,m)) return r;
+    }
+    return -1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    auto S = new Solution();
+    pint(S->strStr("Laanith Dhruvarjun Chouhan",""))
+    return 0;
+}
