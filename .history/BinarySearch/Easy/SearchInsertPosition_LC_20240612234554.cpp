b55b47c941fb8bb3172826618fa7a0dc276427c8
Laanith <lanithchouhan2004@gmail.com>
2024-06-12 23:46:09 +0530

    Latest Commit

---
+++ .history/BinarySearch/Easy/SearchInsertPosition_LC_20240612234554.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define PMINUS cout << -1 << endl
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*/
+
+class Solution
+{
+public:
+    int searchInsert(vi &nums, int target){
+        int left = 0, mid, right = nums.size()-1;
+        if(target>nums[right]) return right+1;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid]<target) left = mid+1;
+            else right = mid-1; 
+        }
+
+        return left;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    auto S = new Solution();
+    vi nums = {1, 3, 5, 6};
+     cout << S->searchInsert(nums, 2);  
+
+    return 0;
+}
