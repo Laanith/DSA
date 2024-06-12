7b87b4a786b3fc5e465722d4f33c23cb35fa7b08
Laanith <lanithchouhan2004@gmail.com>
2024-06-12 12:25:09 +0530

    Latest Commit

---
+++ .history/Hashing/Easy/TwoSum_LC_20240612122415.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define PMINUS cout << -1 << endl
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+class Solution
+{
+public:
+    vi twoSum(vi &nums, int target){
+        map<int,int> numToIndex;
+        for(int i=0; i < nums.size(); i++){
+            int complement = target-nums[i];
+            if(numToIndex.count(complement)) return { numToIndex[complement], i};
+            numToIndex[nums[i]] = i;    
+        }
+
+        return {};
+
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    auto S = new Solution();
+    vi A = {1,2,3,4,5};
+    int target = 9;
+
+
+    return 0;
+}
