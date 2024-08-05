ed91bf660da9adc68023b1be97e474d753741948
Laanith <lanithchouhan2004@gmail.com>
2024-08-05 17:54:47 +0530

    Latest Commit

---
+++ Arrays/Medium/SubarraySumEqualsK_LC.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+ 
+ 
+class Solution{
+public :
+int subArraySum(vi &nums, int k){
+    int count = 0, sum = 0;
+    map<int,int> m;
+    m[0] = 1;
+    for(int i = 0 ; i < nums.size(); i++){
+        sum+=nums[i];
+        count+=m[sum-k];
+        m[sum]++;
+    }
+
+    return count;
+}
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi A = {1,2,3};
+pint(S->subArraySum(A, 3));
+
+ 
+return 0 ;
+}
