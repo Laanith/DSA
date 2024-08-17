f950dff8576480d27b6fc24a70305611e1719713
Laanith <lanithchouhan2004@gmail.com>
2024-08-17 13:05:55 +0530

    Latest Commit

---
+++ Graphs/Easy/DFSOfAGraph_GFG.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+You are given a connected undirected graph. Perform a Depth First Traversal of the graph.
+Note: Use the recursive approach to find the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph.
+
+
+Example 1:
+
+Input: V = 5 , adj = [[2,3,1] , [0], [0,4], [0], [2]]
+
+Output: 0 2 4 3 1
+Explanation: 
+0 is connected to 2, 3, 1.
+1 is connected to 0.
+2 is connected to 0 and 4.
+3 is connected to 0.
+4 is connected to 2.
+so starting from 0, it will go to 2 then 4,
+and then 3 and 1.
+Thus dfs will be 0 2 4 3 1.
+Example 2:
+
+Input: V = 4, adj = [[1,3], [2,0], [1], [0]]
+
+Output: 0 1 2 3
+Explanation:
+0 is connected to 1 , 3.
+1 is connected to 0, 2. 
+2 is connected to 1.
+3 is connected to 0. 
+so starting from 0, it will go to 1 then 2
+then back to 0 then 0 to 3
+thus dfs will be 0 1 2 3. 
+
+Your task:
+You don't need to read input or print anything. Your task is to complete the function dfsOfGraph() which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns a list containing the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph.
+
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V)
+
+
+Constraints:
+1 ≤ V, E ≤ 104
+
+
+*/
+ 
+ 
+class Solution{
+private :
+    void dfs(int node, vi & vis, vi & ls, vi adj[]){
+        vis[node]=1;
+        ls.pb(node);
+        for(int neigh : adj[node]){
+            if(!vis[neigh]) dfs(neigh, vis, ls, adj);
+        }
+        return;
+    }
+public :
+   vi dfsOfGraph(int V, vi adj[]){
+    vi vis(V,0);
+    vi ls;
+    int start = 0;
+    dfs(start, vis, ls, adj);
+    return ls;
+   }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+int V = 5;
+vi adj[] = {{2,3,1} , {0}, {0,4}, {0}, {2}};
+pv(S->dfsOfGraph(V, adj));
+
+ 
+return 0 ;
+}
