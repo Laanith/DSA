c2d6c9955ae5faf383a077e5aa5118c33a424070
Laanith <lanithchouhan2004@gmail.com>
2024-08-23 20:13:17 +0530

    Latest Commit

---
+++ Strings/Medium/ReverseWordsInAString_LC.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+151. Reverse Words in a String
+Medium
+Topics
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+*/
+ 
+ 
+class Solution{
+public :
+    string reverseWords(string s){
+        int n = s.size();
+        int i = n-1;
+        string ans = "", temp = "";
+        while(i>=0){
+            if(s[i]!=' '){
+                temp = s[i]+temp;
+            }
+            else {
+                // cout<<temp<<endl;
+                if(temp!="") ans += (temp + ' ');
+                temp = "";
+            }
+            i--;
+        }
+        if(temp!="") {ans+=temp; return ans;}
+        return ans.substr(0,ans.length()-1);
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout<<S->reverseWords("a good   example")<<endl;
+cout<<S->reverseWords("  hello world  ")<<endl;
+cout<<S->reverseWords("the sky is blue")<<endl;
+
+ 
+return 0 ;
+}
