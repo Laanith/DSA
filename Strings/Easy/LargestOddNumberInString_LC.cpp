6fe6d8173521f5bd4c2f10a3a52281a0f2f8edeb
Laanith <lanithchouhan2004@gmail.com>
2024-08-27 11:37:27 +0530

    Latest Commit

---
+++ Strings/Easy/LargestOddNumberInString_LC.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+903. Largest Odd Number in String
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+ 
+
+Constraints:
+
+1 <= num.length <= 105
+num only consists of digits and does not contain any leading zeros.
+
+*/
+ 
+ 
+class Solution{
+public :
+    string largestOddNumber(string num){
+        int n = num.length();
+        set<char> s = {'1','3','5','7','9'};
+        for(int i = n-1; i >= 0; i--) if(s.find(num[i])!=s.end()) return num.substr(0,i+1);
+        return "";
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout<<S->largestOddNumber("52")<<endl;
+cout<<S->largestOddNumber("4206")<<endl;
+cout<<S->largestOddNumber("35427")<<endl;
+ 
+return 0 ;
+}
