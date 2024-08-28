85ecbff3997224395f3080a94ef9fadef7f038af
Laanith <lanithchouhan2004@gmail.com>
2024-08-28 19:29:17 +0530

    Latest Commit

---
+++ Strings/Easy/MaximumNestingDepthOfTheParantheses_LC.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+1614. Maximum Nesting Depth of the Parentheses
+Solved
+Easy
+Topics
+Companies
+Hint
+Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the maximum number of nested parentheses.
+
+ 
+
+Example 1:
+
+Input: s = "(1+(2*3)+((8)/4))+1"
+
+Output: 3
+
+Explanation:
+
+Digit 8 is inside of 3 nested parentheses in the string.
+
+Example 2:
+
+Input: s = "(1)+((2))+(((3)))"
+
+Output: 3
+
+Explanation:
+
+Digit 3 is inside of 3 nested parentheses in the string.
+
+Example 3:
+
+Input: s = "()(())((()()))"
+
+Output: 3
+
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+It is guaranteed that parentheses expression s is a VPS.
+
+*/
+ 
+ 
+class Solution{
+public :
+    int maxDepth(string s){
+        int maxD = 0, d = 0;
+        for(char x : s){
+            if(x=='('){ d++; maxD = max(maxD, d); }
+            else if(x==')') d--;
+        }
+        return maxD;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+pint(S->maxDepth("(1+(2*3)+((8)/4))+1"));
+
+ 
+return 0 ;
+}
---
+++ Strings/Easy/ValidAnagram_LC.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+242. Valid Anagram
+Solved
+Easy
+Topics
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+ 
+ 
+class Solution{
+public :
+    bool isAnagram(string s, string t){
+        int cnt[26]={0};
+        for(char x : s) cnt[x-'a']++;
+        for(char x : t) cnt[x-'a']--;
+        for(int x : cnt) if(x) return false;
+        return true;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout<<boolalpha<<S->isAnagram("anagram", "nagaram")<<endl;
+cout<<boolalpha<<S->isAnagram("car", "rat")<<endl;
+ 
+return 0 ;
+}
