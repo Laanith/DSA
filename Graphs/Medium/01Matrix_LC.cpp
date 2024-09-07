eb2320a6b6e611ebec5886cd1028d6317525b2bf
Laanith <lanithchouhan2004@gmail.com>
2024-09-07 12:31:13 +0530

    Latest Commit

--- Graphs/Medium/01Matrix_LC.cpp
+++ Graphs/Medium/01Matrix_LC.cpp
@@ -64,14 +64,40 @@ public:
 
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vvi matrix(n, vi(m , -1));
-        for(int i = 0; i < n; i++){
+        vvi vis(n, vi(m,0));
+        vvi dist(n, vi(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0 ; i < n; i++){
             for(int j = 0; j < m; j++){
-                matrix[i][j] = bfs(i,j,mat);
+                if(mat[i][j]==0){
+                    vis[i][j] = 1;
+                    q.push({{i,j}, 0});
+                }
             }
         }
 
-        return matrix;
+        int drow[4] = {0,-1,0,1};
+        int dcol[4] = {1,0,-1,0};
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int step = q.front().second;
+            q.pop();
+            dist[r][c]=step;
+            for(int i= 0; i < 4; i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && vis[nrow][ncol]==0){
+                    q.push({{nrow,ncol},step+1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        return dist;
+
+
     }
 };
  
