11c8af69453a29461870a37a1a887e98f8ea3515
Laanith <lanithchouhan2004@gmail.com>
2024-09-29 23:41:10 +0530

    Latest Commit

---
+++ HeapsandPriorityQueues/Easy/KthLargestElementInAnArray_LC.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+215. Kth Largest Element in an Array
+Solved
+Medium
+Topics
+Companies
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104
+
+*/
+ 
+ 
+class Solution{
+public :
+    int findKthLargest(vi & nums, int k){
+        k--;
+        priority_queue<int> pq;
+        for(int x : nums) pq.push(x);
+        while(k--) pq.pop();
+        return pq.top();
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi nums = {3,2,3,1,2,4,5,5,6};
+pint(S->findKthLargest(nums, 4));
+
+ 
+return 0 ;
+}
