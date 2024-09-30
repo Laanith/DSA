240a1e131c71baa5f3407be0c02acfc782be3f9e
Laanith <lanithchouhan2004@gmail.com>
2024-09-30 19:23:29 +0530

    Latest Commit

---
+++ Graphs/Medium/NumberOfIslands_GFG.cpp
@@ -0,0 +1,220 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+Number Of Islands
+Difficulty: MediumAccuracy: 60.65%Submissions: 42K+Points: 4
+You are given a n,m which means the row and column of the 2D matrix and an array of  size k denoting the number of operations. Matrix elements is 0 if there is water or 1 if there is land. Originally, the 2D matrix is all 0 which means there is no land in the matrix. The array has k operator(s) and each operator has two integer A[i][0], A[i][1] means that you can change the cell matrix[A[i][0]][A[i][1]] from sea to island. Return how many island are there in the matrix after each operation.You need to return an array of size k.
+Note : An island means group of 1s such that they share a common side.
+
+ 
+
+Example 1:
+
+Input: n = 4
+m = 5
+k = 4
+A = {{1,1},{0,1},{3,3},{3,4}}
+
+Output: 1 1 2 2
+Explanation:
+0.  00000
+    00000
+    00000
+    00000
+1.  00000
+    01000
+    00000
+    00000
+2.  01000
+    01000
+    00000
+    00000
+3.  01000
+    01000
+    00000
+    00010
+4.  01000
+    01000
+    00000
+    00011
+ 
+
+ 
+
+Example 2:
+
+Input: n = 4
+m = 5
+k = 4
+A = {{0,0},{1,1},{2,2},{3,3}}
+
+Output: 1 2 3 4
+Explanation:
+0.  00000
+    00000
+    00000
+    00000
+1.  10000
+    00000
+    00000
+    00000
+2.  10000
+    01000
+    00000
+    00000
+3.  10000
+    01000
+    00100
+    00000
+4.  10000
+    01000
+    00100
+    00010
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function numOfIslands() which takes an integer n denoting no. of rows in the matrix, an integer m denoting the number of columns in the matrix and a 2D array of size k denoting  the number of operators.
+
+Expected Time Complexity: O(m * n)
+Expected Auxiliary Space: O(m * n)
+
+Constraints:
+
+1 <= n,m <= 100
+1 <= k <= 1000
+
+
+*/
+
+class DisjointSet{
+    public :
+    vi rank,size,parent;
+        DisjointSet(int n){
+            rank.resize(n+1, 0);
+            size.resize(n+1, 1);
+            parent.resize(n+1);
+            for(int i = 0; i <= n; i++) parent[i] = i;
+        }
+
+        int findParent(int node){
+            if(node==parent[node]) return node;
+            return parent[node] = findParent(parent[node]);
+        }
+
+        void unionBySize(int u, int v){
+            int ultimateParentOfu = findParent(u);
+            int ultimateParentOfv = findParent(v);
+            if(size[ultimateParentOfu] < size[ultimateParentOfv]){
+                parent[ultimateParentOfu] = ultimateParentOfv;
+                size[ultimateParentOfv] += size[ultimateParentOfu];
+            }
+            else {
+                parent[ultimateParentOfv] = ultimateParentOfu;
+                size[ultimateParentOfu] += size[ultimateParentOfv];
+            }
+        }
+
+
+        void unionByRank(int u, int v){
+            int ultimateParentOfu = findParent(u);
+            int ultimateParentOfv = findParent(v);
+            if(ultimateParentOfu == ultimateParentOfv) return;
+            if(rank[ultimateParentOfu] < rank[ultimateParentOfv]){
+                parent[ultimateParentOfu] = ultimateParentOfv;
+            }
+            else if (rank[ultimateParentOfv] < rank[ultimateParentOfu]){
+                parent[ultimateParentOfv] = ultimateParentOfu;
+            }
+            else {
+                parent[ultimateParentOfv] = ultimateParentOfu;
+                rank[ultimateParentOfu]++;
+            }
+        }
+
+};
+ 
+ 
+class Solution{
+private :
+    bool isValid(int adjr, int adjc, int n, int m){
+        return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
+    }
+
+public :
+    vi numOfIslands(int n, int m, vvi & operators){
+        int cnt = 0; 
+        vi ans;
+        int vis[n][m];
+        memset(vis, 0, sizeof vis);
+        int dr[] = {0,1,0,-1};
+        int dc[] = {1,0,-1,0};
+        DisjointSet ds(n*m);
+        for(auto it : operators){
+            int row = it[0];
+            int col = it[1];
+            if(vis[row][col]){
+                ans.push_back(cnt);
+                continue;
+            }
+            vis[row][col] = 1;
+            cnt++;
+            for(int i = 0; i < 4; i++){
+                int adjr = row + dr[i];
+                int adjc = col + dc[i];
+                if(isValid(adjr, adjc, n, m)){
+                    if(vis[adjr][adjc]){
+                        int nodeNo = row * m + col;
+                        int adjNodeNo = adjr * m + adjc;
+                        if(ds.findParent(nodeNo) != ds.findParent(adjNodeNo)){
+                            ds.unionBySize(nodeNo, adjNodeNo);
+                            cnt--;
+                        }
+                    }
+                }
+            }
+
+            ans.push_back(cnt);
+        }
+
+        return ans;
+
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+
+ 
+return 0 ;
+}
