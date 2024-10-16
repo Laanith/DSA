49b1b2bcbf0075f30f3e704ebbae4df1264b72f2
Laanith <lanithchouhan2004@gmail.com>
2024-10-16 16:07:58 +0530

    Latest Commit

---
+++ TwoPointers/Medium/MaximumConsecutiveOnesIII_LC.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+1004. Max Consecutive Ones III
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+*/
+ 
+ 
+class Solution{
+public :
+    int longestOnes(vi & arr, int k){
+        int i  = 0, j = 0, cnt = 0, maxl = imi;
+        while(j< arr.size()){
+            if(!arr[j]) cnt++;
+
+
+            while(cnt>k){
+                if(!arr[i]) cnt--;
+                i++;
+            }
+            maxl = max(maxl, j-i+1);
+
+
+            j++;
+        }
+
+        return maxl;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi A = {1,1,1,0,0,0,1,1,1,1,0};
+pint(S->longestOnes(A, 3));
+ 
+return 0 ;
+}
