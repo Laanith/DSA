b3323b7fa0367c9cedf0eb44318a0b6f653f21d2
Laanith <lanithchouhan2004@gmail.com>
2024-10-20 19:42:10 +0530

    Latest Commit

---
+++ Strings/ZigZagConversion_LC.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+6. Zigzag Conversion
+Solved
+Medium
+Topics
+Companies
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+*/
+ 
+ 
+class Solution{
+public :
+    string convert(string s, int numRows){
+        if(numRows <= 1) return s;
+        int dir = -1,j=0;
+        vector<string> v(numRows, "");
+        for(int i=0;i < s.length(); i++){
+            if(j==numRows-1 || j ==0) dir*=(-1);
+            v[j] += s[i];
+            if(dir==1) j++;
+            else j--;
+        }
+        string ans;
+        for(string t : v) ans += t;
+        return ans;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout<<S->convert("PAYPALISHIRING",3)<<endl;
+cout<<S->convert("LAANITH",3)<<endl;
+
+
+ 
+return 0 ;
+}
