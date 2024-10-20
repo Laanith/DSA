750032920a4a6276d13c78a1dd239e16fb152329
Laanith <lanithchouhan2004@gmail.com>
2024-10-20 22:57:48 +0530

    Latest Commit

---
+++ NumberTheoryandMath/Medium/ReverseInteger_LC.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+7. Reverse Integer
+Solved
+Medium
+Topics
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*/
+ 
+ 
+class Solution{
+public :
+    int reverse(int x){
+        int t = 0;
+        while(x){
+            int d = x%10;
+            if(t<imi/10 || t > ima/10) return 0;
+            t*=10;
+            t+=d;
+            x/=10;
+        }
+        return t;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+pint(S->reverse(12119208));
+
+ 
+return 0 ;
+}
