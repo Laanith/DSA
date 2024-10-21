e1252b2d1a2ad335d6fd99873bda22d8ab79f09b
Laanith <lanithchouhan2004@gmail.com>
2024-10-21 12:57:12 +0530

    Latest Commit

---
+++ BinarySearch/Medium/H-Index_LC.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+274. H-Index
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+ 
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+ 
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+*/
+ 
+ 
+class Solution{
+private:
+    bool check(vi & citations, int mid){
+        int cnt=0;
+        for(int it : citations) if(it>=mid) cnt++;
+        return cnt>=mid;
+    }
+public :
+    int HIndex(vi & citations){
+        int l = 0, h = citations.size(),ans;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(check(citations,mid)){l = mid+1; ans=mid;}
+            else h = mid-1;
+        }
+        return ans;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+vi A = {3,0,6,1,5};
+pint(S->HIndex(A));
+A = {1,3,1};
+pint(S->HIndex(A));
+
+return 0 ;
+}
