4c2e44a8e0fee562af796ec550d74933d76f54aa
Laanith <lanithchouhan2004@gmail.com>
2024-10-21 17:58:59 +0530

    Latest Commit

---
+++ NumberTheoryandMath/Medium/GlobalAndLocalInversions_LC.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+775. Global and Local Inversions
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an integer array nums of length n which represents a permutation of all the integers in the range [0, n - 1].
+
+The number of global inversions is the number of the different pairs (i, j) where:
+
+0 <= i < j < n
+nums[i] > nums[j]
+The number of local inversions is the number of indices i where:
+
+0 <= i < n - 1
+nums[i] > nums[i + 1]
+Return true if the number of global inversions is equal to the number of local inversions.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,2]
+Output: true
+Explanation: There is 1 global inversion and 1 local inversion.
+Example 2:
+
+Input: nums = [1,2,0]
+Output: false
+Explanation: There are 2 global inversions and 1 local inversion.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+0 <= nums[i] < n
+All the integers of nums are unique.
+nums is a permutation of all the numbers in the range [0, n - 1].
+
+*/
+ 
+ 
+class Solution{
+public :
+    bool isIdealPermutation(vi nums){
+        int n = nums.size();
+        int max_so_far = nums[0];
+        for(int i = 1; i < n; i++){
+            if(max_so_far > nums[i+1]) return false;
+            max_so_far = max(max_so_far, nums[i]);
+        }
+        return true;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+cout<<boolalpha<<S->isIdealPermutation({1,2,0})<<endl;
+cout<<boolalpha<<S->isIdealPermutation({1,0,2})<<endl;
+
+
+ 
+return 0 ;
+}
