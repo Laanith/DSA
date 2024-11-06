a8c14b2a199e4b4bf55605de75ee1b9e2cf8d347
Laanith <lanithchouhan2004@gmail.com>
2024-11-07 03:53:44 +0530

    Latest Commit

---
+++ BitManipulation/Easy/HammingDistance_LC.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define ll long long
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define PMINUS cout<<-1<<endl
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define seti set<int>
+#define unseti unordered_set<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+void pvvi(vvi A){for(auto v : A){pv(v);}cout << endl;}
+
+
+
+ 
+ 
+/*!SECTION : Problem Description ::
+461. Hamming Distance
+Easy
+Topics
+Companies
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+ 
+
+Example 1:
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+Example 2:
+
+Input: x = 3, y = 1
+Output: 1
+ 
+
+Constraints:
+
+0 <= x, y <= 231 - 1
+ 
+
+Note: This question is the same as 2220: Minimum Bit Flips to Convert Number.
+*/
+ 
+ 
+class Solution{
+public :
+    int hammingDistance(int x, int y){
+        int t = x^y;
+        int cnt = 0;
+        while(t){
+            cnt += t&1;
+            t = t>>1;
+        }
+        return cnt;
+    }
+};
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+auto S = new Solution();
+pint(S->hammingDistance(3,4));
+pint(S->hammingDistance(3,5));
+
+
+ 
+return 0 ;
+}
